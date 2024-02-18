99727240d213053f4b054cbe50360e4f43f4adf0
Mac <mac@Macs-MacBook-Air.local>
2024-02-17 23:22:45 -0800

    script changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // retries:
+  // {
+  //   runmode: 1
+  // },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    projectId: "gxz56d",
     specPattern:"cypress/integration/practice/*.js"
   },
 });
