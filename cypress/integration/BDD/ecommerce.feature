fa8c93fa9715df62fd9fd7b2c43a364b35b4125d
Mac <mac@Macs-MacBook-Air.local>
2024-03-02 03:56:52 -0800

    adding html reports

--- cypress/integration/BDD/ecommerce.feature
+++ cypress/integration/BDD/ecommerce.feature
@@ -3,5 +3,7 @@ Feature: Ecommerce E2E vaidation
     Feature testing of Ecommerce
     Scenario: Ecommerce product delivery
     Given I open Ecommerce page
+        # | name        | email                   | password | Gender | Date       |
+        # | Chetanachar | chetanachar.k@gmail.com | 123      | Male   | 1995/07/05 |
     When Start to type your When step here I fill the form
     Then should show success message
--- cypress/integration/BDD/ecommerce/ecomStepDef.js
+++ cypress/integration/BDD/ecommerce/ecomStepDef.js
@@ -2,20 +2,38 @@ import { Given,when,And,Then, When } from "@badeball/cypress-cucumber-preprocess
 //import { when } from "cypress/types/jquery";
 
 
+import HomePage from "../../pageObjects/Home";
+//import { data } from "cypress/types/jquery";
+const homePage = new HomePage()
+before(function(){
+    cy.fixture('example').then (function(data){
+       this.data = data
+    })
+})
 Given("I open Ecommerce page", () => { 
     cy.visit("https://rahulshettyacademy.com/angularpractice/")
+
 })
 
-When("Start to type your When step here I fill the form",()=>{
-    cy.get(':nth-child(1) > .form-control').type("chetanachar") 
-    cy.get(".btn").click()
+When("Start to type your When step here I fill the form",function(){
+    // cy.log("Received dataTable:", dataTable);
+    // cy.log("Raw Table:", dataTable.raw());
+
+
+    homePage.getEditBox().type(this.data.name)
+    homePage.getEmailbox().type(this.data.email)
+    homePage.getPwdBox().type(this.data.pwd)
+    homePage.getCheckMe().check()
+    homePage.getGender().select(this.data.gender)
+    homePage.getDateBox().type(this.data.date)
+    homePage.getSubmitbutton().click()
 
 })
 // And ("click on submit", ()=> {
 //     cy.get(".btn").click()
 // })
 Then("should show success message",()=> {
-    cy.get('.alert').then(function(elememt){
+    homePage.getMessage().then(function(elememt){
         const messageText = elememt.text()
         expect(messageText.includes("Success")).to.be.true
 
